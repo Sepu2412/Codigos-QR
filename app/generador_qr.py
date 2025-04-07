a732da36decbb60d59999dda75c4aad3aafbb6d2
Sebastián Villa <sebasviro567@gmail.com>
2025-04-06 23:28:13 -0500

    Comienzo del método generar_qr

--- app/generador_qr.py
+++ app/generador_qr.py
@@ -1,3 +1,8 @@
+import qrcode
+from PIL import Image
+import os
+import datetime
+
 class QRGenerator:
     def __init__(self, contenido: str, color: str = "black", logo = None, tamaño: int = 300):
         self.contenido: str = contenido
@@ -6,7 +11,12 @@ class QRGenerator:
         self.tamaño : int = tamaño
 
     def generar_qr(self):
-        pass
+        qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+        qr.add_data(self.contenido)
+        qr.make()
+        img = qr.make_image(fill_color=self.color, back_color="white").convert('RGB')
+        img = img.resize((self.tamaño, self.tamaño))
+
 
     def guardar_qr(self, ruta: str):
         pass
