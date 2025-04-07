0af099f11b46fe5412182460f942f89d615ca9c1
tomas sepulveda <sepulvedatomas1027@gmail.com>
2025-04-06 23:40:26 -0500

    implementacion del menu

--- app/main.py
+++ app/main.py
@@ -2,3 +2,12 @@ from generador_qr import QRGenerator
 from lector_qr import QRReader
 import os
 import time
+
+def menu_principal():
+    while True:
+        print("\n--- Generador y Lector de QR ---")
+        print("1. Generar código QR")
+        print("2. Leer código QR desde imagen")
+        print("3. Salir")
+
+        opcion = input("Selecciona una opción: ")
