a521e820428385cdbced681bb258b5ea30578119
Sebastián Villa <sebasviro567@gmail.com>
2025-05-20 22:02:14 -0500

    Añadir metodo, buscar qr en Clase QRHistory

--- app/historial.py
+++ app/historial.py
@@ -1,16 +1,15 @@
 from typing import List, Dict, Any
+from datetime import datetime
 
 class QRHistory:
     def __init__(self):
         self.lista_qr: List[Dict[str, Any]] = []
 
     def buscar_qr(self, texto: str) -> list:
-        pass
-
-    
+        return [qr for qr in self.lista_qr if texto.lower() in qr.get("contenido", "").lower()]
 
     def filtrar_qr_por_tipo(self, tipo: str) -> list:
         pass
 
     def limpiar_historial(self):
-        pass
+       pass
