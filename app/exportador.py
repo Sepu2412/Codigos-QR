4a0d23fef3c1a03272058fbfb351f8b97864efae
Sebastián Villa <sebasviro567@gmail.com>
2025-05-21 20:25:53 -0500

    Añadir métodos (guardar como png o jpg) en clase QRExporter

--- app/exportador.py
+++ app/exportador.py
@@ -1,14 +1,22 @@
-#from PIL import Image
+from PIL import Image
+import os
+import platform
+
 
 class QRExporter:
     def __init__(self, ruta_guardado: str):
         self.ruta_guardado = ruta_guardado
 
-    #def guardar_como_png(self, imagen_qr: Image.Image, nombre_archivo: str):
-        pass
+    def guardar_como_png(self, imagen_qr: Image.Image, nombre_archivo: str):
+        ruta_completa = os.path.join(self.ruta_guardado, f"{nombre_archivo}.png")
+        imagen_qr.save(ruta_completa, format='PNG')
+        print(f"✅ QR guardado como PNG en: {ruta_completa}")
 
-    #def guardar_como_jpg(self, imagen_qr: Image.Image, nombre_archivo: str):
-        pass
+    def guardar_como_jpg(self, imagen_qr: Image.Image, nombre_archivo: str):
+        if imagen_qr.mode != 'RGB':
+            imagen_qr = imagen_qr.convert('RGB')  # JPG no soporta transparencia
+        ruta_completa = os.path.join(self.ruta_guardado, f"{nombre_archivo}.jpg")
+        imagen_qr.save(ruta_completa, format='JPEG')
+        print(f"✅ QR guardado como JPG en: {ruta_completa}")
 
-    #def copiar_al_portapapeles(self, imagen_qr: Image.Image):
-        pass
+
